abd026e22b93a8e9a018b99fd03619285f874b9d
Arthur David Olson <ado@elsie>
1992-12-01 15:41:33 -0500

    delinted and bullet-proofed
    SCCS-file: asctime.c
    SCCS-SID: 7.2

--- asctime.c
+++ asctime.c
@@ -24,7 +24,14 @@ register const struct tm *	timeptr;
 		"Jan", "Feb", "Mar", "Apr", "May", "Jun",
 		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 	};
-	static char		result[26];
+	/*
+	** Big enough for
+	** ??? ???-2147483648 -2147483648:-2147483648:-2147483648 -2147483648\n
+	** (two three-character abbreviations, five eleven-character numbers,
+	** three explicit spaces, two explicit colons, a newline,
+	** and a trailing ASCII nul.)
+	*/
+	static char		result[3 * 2 + 5 * 11 + 3 + 2 + 1 + 1];
 	register const char *	wn;
 	register const char *	mn;
 
@@ -34,7 +41,13 @@ register const struct tm *	timeptr;
 	if (timeptr->tm_mon < 0 || timeptr->tm_mon >= MONSPERYEAR)
 		mn = "???";
 	else	mn = mon_name[timeptr->tm_mon];
-	(void) sprintf(result, "%.3s %.3s%3d %02.2d:%02.2d:%02.2d %d\n",
+	/*
+	** The X3J11-suggested format is
+	**	"%.3s %.3s%3d %02.2d:%02.2d:%02.2d %d\n"
+	** but, given the way we've set wn and mn, the .3's are superfluous.
+	** Meanwhile, the .2 in 02.2d is ignored, so we drop it.
+	*/
+	(void) sprintf(result, "%s %s%3d %02d:%02d:%02d %d\n",
 		wn, mn,
 		timeptr->tm_mday, timeptr->tm_hour,
 		timeptr->tm_min, timeptr->tm_sec,

4b514d734942a757fb1e3b498459fa5277018549
Arthur David Olson <ado@elsie>
1994-08-20 13:17:29 -0400

    _t -> _T; P() redefined
    SCCS-file: private.h
    SCCS-SID: 7.8

--- private.h
+++ private.h
@@ -75,20 +75,19 @@ static char	privatehid[] = "%W%";
 #define P(x)	x
 #endif /* defined __STDC__ */
 #ifndef __STDC__
-#define ASTERISK	*
-#define P(x)	( /ASTERISK x ASTERISK/ )
+#define P(x)	()
 #endif /* !defined __STDC__ */
 #endif /* !defined P */
 
 /*
-** genericptr_t
+** genericptr_T
 */
 
 #ifdef __STDC__
-typedef void *		genericptr_t;
+typedef void *		genericptr_T;
 #endif /* defined __STDC__ */
 #ifndef __STDC__
-typedef char *		genericptr_t;
+typedef char *		genericptr_T;
 #endif /* !defined __STDC__ */
 
 #include "sys/types.h"	/* for time_t */
@@ -133,29 +132,29 @@ extern int	unlink P((const char * filename));
 
 #ifdef __STDC__
 
-#define alloc_size_t	size_t
-#define qsort_size_t	size_t
-#define fwrite_size_t	size_t
+#define alloc_size_T	size_t
+#define qsort_size_T	size_t
+#define fwrite_size_T	size_t
 
 #endif /* defined __STDC__ */
 #ifndef __STDC__
 
-#ifndef alloc_size_t
-#define alloc_size_t	unsigned
-#endif /* !defined alloc_size_t */
+#ifndef alloc_size_T
+#define alloc_size_T	unsigned
+#endif /* !defined alloc_size_T */
 
-#ifndef qsort_size_t
+#ifndef qsort_size_T
 #ifdef USG
-#define qsort_size_t	unsigned
+#define qsort_size_T	unsigned
 #endif /* defined USG */
 #ifndef USG
-#define qsort_size_t	int
+#define qsort_size_T	int
 #endif /* !defined USG */
-#endif /* !defined qsort_size_t */
+#endif /* !defined qsort_size_T */
 
-#ifndef fwrite_size_t
-#define fwrite_size_t	int
-#endif /* !defined fwrite_size_t */
+#ifndef fwrite_size_T
+#define fwrite_size_T	int
+#endif /* !defined fwrite_size_T */
 
 #ifndef USG
 extern char *		sprintf P((char * buf, const char * format, ...));
@@ -168,14 +167,14 @@ extern char *		sprintf P((char * buf, const char * format, ...));
 */
 
 extern char *		getenv P((const char * name));
-extern genericptr_t	malloc P((alloc_size_t size));
-extern genericptr_t	calloc P((alloc_size_t nelem, alloc_size_t elsize));
-extern genericptr_t	realloc P((genericptr_t oldptr, alloc_size_t newsize));
+extern genericptr_T	malloc P((alloc_size_T size));
+extern genericptr_T	calloc P((alloc_size_T nelem, alloc_size_T elsize));
+extern genericptr_T	realloc P((genericptr_T oldptr, alloc_size_T newsize));
 
 #ifdef USG
 extern void		exit P((int s));
-extern void		qsort P((genericptr_t base, qsort_size_t nelem,
-				qsort_size_t elsize, int (*comp)()));
+extern void		qsort P((genericptr_T base, qsort_size_T nelem,
+				qsort_size_T elsize, int (*comp)()));
 extern void		perror P((const char * string));
 extern void		free P((char * buf));
 #endif /* defined USG */

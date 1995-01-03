494013dd0e177b0a021bdb1e4c887f55087c2f30
Arthur David Olson <ado@elsie>
1995-01-03 11:17:05 -0500

    space mods
    SCCS-file: localtime.c
    SCCS-SID: 7.31

--- localtime.c
+++ localtime.c
@@ -1197,8 +1197,8 @@ increment_overflow(number, delta)
 int *	number;
 int	delta;
 {
-	int number0;
-	
+	int	number0;
+
 	number0 = *number;
 	*number += delta;
 	return (*number < number0) != (delta < 0);

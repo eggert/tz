efbbcfd1c6c5318f20fbf1d5440599b1d684177e
Arthur David Olson <ado@elsie>
2000-04-20 15:36:18 -0400

    void return bug fix
    SCCS-file: date.c
    SCCS-SID: 7.35

--- date.c
+++ date.c
@@ -576,8 +576,9 @@ const struct tm * const	tmp;
 			errensure();
 			(void) exit(retval);
 		}
+		cp[0] = '\1';
 		result = strftime(cp, size, format, tmp);
-		if (result != 0)
+		if (result != 0 || cp[0] == '\0')
 			break;
 		size += INCR;
 		cp = realloc(cp, (size_t) size);

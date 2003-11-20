358047d36e4624873bb5bfd9df5c494de1daf00f
Arthur David Olson <olsona@elsie>
2003-11-20 08:51:04 -0500

    SCCS-file: workman.sh SCCS-SID: 1.5

--- workman.sh
+++ workman.sh
@@ -3,22 +3,23 @@
 # %W%
 
 echo ".hy 0
-.pl 99i
-.na" |
-	nroff -man - ${1+"$@"} |
-	perl -ne '
-		chomp;
-		s/.\010//g;
-		s/[ 	]*$//;
-		if (/^$/) {
-			$sawblank = 1;
-			next;
-		} else {
-			if ($sawblank && $didprint) {
-				print "\n";
-				$sawblank = 0;
-			}
-			print "$_\n";
-			$didprint = 1;
+.na
+.de }H
+..
+.de }F
+.." | nroff -man - ${1+"$@"} | perl -ne '
+	chomp;
+	s/.\010//g;
+	s/\s*$//;
+	if (/^$/) {
+		$sawblank = 1;
+		next;
+	} else {
+		if ($sawblank && $didprint) {
+			print "\n";
+			$sawblank = 0;
 		}
-	' 
+		print "$_\n";
+		$didprint = 1;
+	}
+' 

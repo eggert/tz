b91421fd8dfee2b6f9bb9c6a08b723e9595ea836
Arthur David Olson <olsona@elsie>
2003-11-20 08:43:17 -0500

    simplified
    SCCS-file: workman.sh
    SCCS-SID: 1.4

--- workman.sh
+++ workman.sh
@@ -2,39 +2,23 @@
 
 # %W%
 
-tty -s
-ttyval=$?
-
-case $# in
-	0)	nroff -man ;;
-	1)	if [ -f $1 ]
-		then
-			( echo .hy 0; echo .na ) | nroff -man - "$1"
-		else
-			man "$1"
-		fi ;;
-	*)	man ${1+"$@"} ;;
-esac | perl -ne '
-	if (($. % 66) <= 7) {
-		next;
-	}
-	if (($. % 66) > (66 - 7)) {
-		next;
-	}
-	chomp;
-	s/.\010//g;
-	s/[ 	]*$//;
-	if (/^$/) {
-		$sawblank = 1;
-		next;
-	} else {
-		if ($sawblank) {
-			print "\n";
-			$sawblank = 0;
+echo ".hy 0
+.pl 99i
+.na" |
+	nroff -man - ${1+"$@"} |
+	perl -ne '
+		chomp;
+		s/.\010//g;
+		s/[ 	]*$//;
+		if (/^$/) {
+			$sawblank = 1;
+			next;
+		} else {
+			if ($sawblank && $didprint) {
+				print "\n";
+				$sawblank = 0;
+			}
+			print "$_\n";
+			$didprint = 1;
 		}
-		print "$_\n";
-	}
-' | case $ttyval in
-	0)	more ;;
-	*)	cat ;;
-esac
+	' 

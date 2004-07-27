f9a007fe6ea3449f337ec03342563a7ad0037d19
Arthur David Olson <olsona@elsie>
2004-07-27 10:45:55 -0400

    SCCS-file: asctime.c SCCS-SID: 7.15

--- asctime.c
+++ asctime.c
@@ -34,7 +34,15 @@ char *				buf;
 	};
 	register const char *	wn;
 	register const char *	mn;
-	register int		result;
+	/*
+	** Big enough for something such as
+	** ??? ???-2147483648 -2147483648:-2147483648:-2147483648 -2147483648\n
+	** (two three-character abbreviations, five strings denoting integers,
+	** three explicit spaces, two explicit colons, a newline,
+	** and a trailing ASCII nul).
+	*/
+	char			result[2 * 3 + 5 * INT_STRLEN_MAXIMUM(int) +
+					3 + 2 + 1 + 1];
 
 	if (timeptr->tm_wday < 0 || timeptr->tm_wday >= DAYSPERWEEK)
 		wn = "???";
@@ -46,24 +54,30 @@ char *				buf;
 	** The format used in the (2004) standard is
 	**	"%.3s %.3s%3d %.2d:%.2d:%.2d %d\n"
 	** Some systems only handle "%.2d"; others only handle "%02d";
-	** "%02.2d" makes everybody happy.
+	** "%02.2d" makes (most) everybody happy.
+	** All years associated with 32-bit time_t values are exactly
+	** four digits long; some years associated with 64-bit time_t
+	** values are not four digits long so we throw in the 4 below.
 	*/
-	result = snprintf(buf, STANDARD_BUFFER_SIZE,
-		"%.3s %.3s%3d %02.2d:%02.2d:%02.2d %4ld\n",
+	/*
+	** We avoid using snprintf since it's not available on all systems.
+	*/
+	(void) sprintf(result, "%.3s %.3s%3d %02.2d:%02.2d:%02.2d %4ld\n",
 		wn, mn,
 		timeptr->tm_mday, timeptr->tm_hour,
 		timeptr->tm_min, timeptr->tm_sec,
 		timeptr->tm_year + (long) TM_YEAR_BASE);
-	if (result < 0 || result >= STANDARD_BUFFER_SIZE) {
+	if (strlen(result) >= STANDARD_BUFFER_SIZE) {
 		errno = EOVERFLOW;
 		return NULL;
+	} else {
+		(void) strcpy(buf, result);
+		return buf;
 	}
-	return buf;
 }
 
 /*
-** A la ISO/IEC 9945-1, ANSI/IEEE Std 1003.1, 2004 Edition,
-** with core dump avoidance.
+** A la ISO/IEC 9945-1, ANSI/IEEE Std 1003.1, 2004 Edition.
 */
 
 char *

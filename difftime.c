e0ba4abff7276cf2fb60e022cf7d242364686612
Arthur David Olson <olsona@elsie>
2004-10-21 10:29:28 -0400

    difftime work
    SCCS-file: difftime.c
    SCCS-SID: 7.10

--- difftime.c
+++ difftime.c
@@ -11,73 +11,45 @@ static char	elsieid[] = "%W%";
 
 /*LINTLIBRARY*/
 
-#include "private.h"
-
-/*
-** Algorithm courtesy Paul Eggert (eggert@twinsun.com).
-*/
-
-#ifdef HAVE_LONG_DOUBLE
-#define long_double	long double
-#endif /* defined HAVE_LONG_DOUBLE */
-#ifndef HAVE_LONG_DOUBLE
-#define long_double	double
-#endif /* !defined HAVE_LONG_DOUBLE */
+#include "sys/types.h"	/* for time_t */
+#include "private.h"	/* for TYPE_INTEGRAL and TYPE_SIGNED */
 
 double
 difftime(time1, time0)
 const time_t	time1;
 const time_t	time0;
 {
-	time_t	delta;
-	time_t	hibit;
+	if (!TYPE_INTEGRAL(time_t)) {
+		/*
+		** time_t is floating.
+		** Do the math in whichever of time_t or double is wider.
+		*/
+		if (sizeof (time_t) >= sizeof (double))
+			return time1 - time0;
+		else	return (double) time1 - (double) time0;
+	} else if (!TYPE_SIGNED(time_t)) {
+		/*
+		** time_t is integral and unsigned.
+		** The difference of two time_t's won't overflow if
+		** the minuend is greater than or equal to the subtrahend.
+		*/
+		if (time1 >= time0)
+			return time1 - time0;
+		else	return -((double) (time0 - time1));
+	} else {
+		/*
+		** time_t is integral and signed.
+		** As elsewhere in the time zone package,
+		** use modular arithmetic to avoid overflow.
+		** We could check to see if double is sufficiently wider
+		** than time_t to let us simply return
+		**	(double) time1 - (double) time0
+		*/
+		register time_t	lead;
+		register time_t	trail;
 
-	{
-		time_t		tt;
-		double		d;
-		long_double	ld;
-
-		if (sizeof tt < sizeof d)
-			return (double) time1 - (double) time0;
-		if (sizeof tt < sizeof ld)
-			return (long_double) time1 - (long_double) time0;
+		lead = time1 / 2 - time0 / 2;
+		trail = time1 % 2 - time0 % 2;
+		return 2 * ((double) lead) + trail;
 	}
-	if (time1 < time0)
-		return -difftime(time0, time1);
-	/*
-	** As much as possible, avoid loss of precision
-	** by computing the difference before converting to double.
-	*/
-	delta = time1 - time0;
-	if (delta >= 0)
-		return delta;
-	/*
-	** Repair delta overflow.
-	*/
-	hibit = (~ (time_t) 0) << (TYPE_BIT(time_t) - 1);
-	/*
-	** The following expression rounds twice, which means
-	** the result may not be the closest to the true answer.
-	** For example, suppose time_t is 64-bit signed int,
-	** long_double is IEEE 754 double with default rounding,
-	** time1 = 9223372036854775807 and time0 = -1536.
-	** Then the true difference is 9223372036854777343,
-	** which rounds to 9223372036854777856
-	** with a total error of 513.
-	** But delta overflows to -9223372036854774273,
-	** which rounds to -9223372036854774784, and correcting
-	** this by subtracting 2 * (long_double) hibit
-	** (i.e. by adding 2**64 = 18446744073709551616)
-	** yields 9223372036854776832, which
-	** rounds to 9223372036854775808
-	** with a total error of 1535 instead.
-	** This problem occurs only with very large differences.
-	** It's too painful to fix this portably.
-	** We are not alone in this problem;
-	** some C compilers round twice when converting
-	** large unsigned types to small floating types,
-	** so if time_t is unsigned the "return delta" above
-	** has the same double-rounding problem with those compilers.
-	*/
-	return delta - 2 * (long_double) hibit;
 }

c554d2f7e53757a03316816974524663943f6acf
Arthur David Olson <olsona@elsie>
2004-10-25 12:50:55 -0400

    SCCS-file: difftime.c SCCS-SID: 7.12

--- difftime.c
+++ difftime.c
@@ -22,23 +22,40 @@ const time_t	time0;
 	if (!TYPE_INTEGRAL(time_t)) {
 		/*
 		** time_t is floating.
-		** We can't apply % to floats.
-		** Do the math in whichever of time_t or double is wider.
+		** Do the math in whichever of time_t or double is wider
+		** (assuming that the wider type has more precision).
 		*/
 		if (sizeof (time_t) >= sizeof (double))
 			return time1 - time0;
 		else	return (double) time1 - (double) time0;
+	} else if (!TYPE_SIGNED(time_t)) {
+		/*
+		** time_t is integral and unsigned.
+		** The difference of two unsigned values can't overflow
+		** if the minuend is greater than or equal to the subtrahend.
+		*/
+		if (time1 >= time0)
+			return time1 - time0;
+		else	return -((double) (time0 - time1));
 	} else {
 		/*
-		** time_t is integral.
-		** As elsewhere in the time zone package,
-		** use modular arithmetic to avoid overflow.
+		** time_t is integral and signed.
+		** If a double is much wider than a time_t,
+		** simply convert and subtract.
 		*/
-		register time_t	lead;
-		register time_t	trail;
+		if (sizeof (double) >= 2 * sizeof (time_t))
+			return (double) time1 - (double) time0;
+		else {
+			/*
+			** As elsewhere in the time zone package,
+			** use modular arithmetic to avoid overflow.
+			*/
+			register time_t	lead;
+			register time_t	trail;
 
-		lead = time1 / 2 - time0 / 2;
-		trail = time1 % 2 - time0 % 2;
-		return 2 * ((double) lead) + trail;
+			lead = time1 / 2 - time0 / 2;
+			trail = time1 % 2 - time0 % 2;
+			return 2.0 * lead + trail;
+		}
 	}
 }

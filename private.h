b3c830983690c419f0a91b8d0a2e104507435fad
Paul Eggert <eggert@cs.ucla.edu>
2014-08-22 17:47:08 -0700

    * private.h (asctime_r): Qualify arguments with 'restrict'.
    This is for compatibility with POSIX.

--- private.h
+++ private.h
@@ -308,7 +308,7 @@ time_t time(time_t *);
 */
 
 #ifndef asctime_r
-extern char *	asctime_r(struct tm const *, char *);
+extern char *	asctime_r(struct tm const *restrict, char *restrict);
 #endif
 
 /*

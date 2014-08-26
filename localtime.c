3230f367b09e5b9476253176fa51123e90acb5d2
Paul Eggert <eggert@cs.ucla.edu>
2014-08-26 12:48:16 -0700

    * localtime.c (zoneinit, tzalloc): Return NULL on failure.
    Suggested by Jonathan Lennox in:
    http://mm.icann.org/pipermail/tz/2014-August/021529.html

--- localtime.c
+++ localtime.c
@@ -1191,7 +1191,7 @@ zoneinit(struct state *sp, char const *name)
       strcpy(sp->chars, gmt);
     } else if (! (tzload(name, sp, true)
 		  || (name && name[0] != ':' && tzparse(name, sp, false))))
-      gmtload(sp);
+      return NULL;
   }
   return sp;
 }
@@ -1264,7 +1264,10 @@ timezone_t
 tzalloc(char const *name)
 {
   timezone_t sp = malloc(sizeof *sp);
-  return zoneinit(sp, name);
+  timezone_t tp = sp ? zoneinit(sp, name) : sp;
+  if (!tp)
+    free(sp);
+  return tp;
 }
 
 void

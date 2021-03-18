b4702636c7792b6d4e329f06b53ed276f9e013ad
Paul Eggert <eggert@cs.ucla.edu>
2021-03-18 12:14:26 -0700

    Fix and simplify year-cycle reduction
    * zdump.c (delta_nonneg): Speed up code by using division
    rather than repeated addition.  Based on a suggestion from
    Arthur David Olson; this version avoids int overflow.

--- zdump.c
+++ zdump.c
@@ -693,11 +693,11 @@ hunt(timezone_t tz, char *name, time_t lot, time_t hit)
 static intmax_t
 delta_nonneg(struct tm *newp, struct tm *oldp)
 {
-	register intmax_t	result;
-	register int		tmy;
-
-	result = 0;
-	for (tmy = oldp->tm_year; tmy < newp->tm_year; ++tmy)
+	intmax_t oldy = oldp->tm_year;
+	int cycles = (newp->tm_year - oldy) / YEARSPERREPEAT;
+	intmax_t sec = SECSPERREPEAT, result = cycles * sec;
+	int tmy = oldp->tm_year + cycles * YEARSPERREPEAT;
+	for ( ; tmy < newp->tm_year; ++tmy)
 		result += DAYSPERNYEAR + isleap_sum(tmy, TM_YEAR_BASE);
 	result += newp->tm_yday - oldp->tm_yday;
 	result *= HOURSPERDAY;
